51a6bdb2e8546b41c746a87743b31e6567188371
Pattan Rasheeda <pattanrasheeda786@gmail.com>
2026-07-12 10:39:19 +0530

    Update MongoDB connection URI environment variable

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ app.use(cors());
 app.use(express.json());
 
 // ➕ Connect to MongoDB
-mongoose.connect(process.env.MONGO_URL, {
+mongoose.connect(process.env.MONGO_URI, { {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
